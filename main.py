c89fc678da616be661231c15e3e015d01e5a7a6a
Kevin Blansit <kevin.blansit@gmail.com>
2017-04-05 12:01:32 -0700

    read dicome locations

--- main.py
+++ main.py
@@ -2,5 +2,19 @@
 
 # import libraries
 import numpy as np
-
 import dicom
+import os
+
+# main
+def main():
+    # temp path
+    tmp_path = 'data/ex1'
+
+    # store files
+    dicom_files = os.listdir(tmp_path)
+
+
+    import pdb; pdb.set_trace()
+
+if __name__ == '__main__':
+    main()
