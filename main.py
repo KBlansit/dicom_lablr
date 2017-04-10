1cb3694179cd0394f9679f24c499f876ca4c75cc
Kevin Blansit <kevin.blansit@gmail.com>
2017-04-10 13:40:27 -0700

    fixed path

--- main.py
+++ main.py
@@ -4,17 +4,26 @@
 import os
 import dicom
 import numpy
+import argparse
 
 from matplotlib import pyplot, cm
 
 # main
 def main():
-    # temp path
-    tmp_path = 'data/ex1'
+    # pass command line args
+    cmd_parse = argparse.ArgumentParser(description = 'Application for scoring dicom files')
+    cmd_parse.add_argument('-p', '--path', help = 'path for input dicom files', type=str)
+    cmd_args = cmd_parse.parse_args()
+
+    # check command line args
+    if cmd_args.path is None:
+        raise AssertionError("No path specified")
+    elif not os.path.exists(cmd_args.path):
+        raise AssertionError("Cannot locate path: " + cmd_args.path)
 
     # store files and append path
-    dicom_files = os.listdir(tmp_path)
-    dicom_files = [tmp_path + "/" + x for x in dicom_files]
+    dicom_files = os.listdir(cmd_args.path)
+    dicom_files = [cmd_args.path + "/" + x for x in dicom_files]
 
     # read dicom files
     dicom_obj = [dicom.read_file(x, force=True) for x in dicom_files]
---
+++ testRun.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# import libraries
+import os
+
+# define parts of program call
+base_call = './main.py'
+path_call = '-p data/ex1'
+
+# make into a list
+cmd_lst = [
+    base_call,
+    path_call,
+]
+
+# source and run file
+os.system("chmod +x " + base_call)
+os.system(" ".join(cmd_lst))
