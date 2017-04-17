770c20aa416a4d59a4950041df87a6c981049415
Kevin Blansit <kevin.blansit@gmail.com>
2017-04-16 22:50:14 -0700

    added basics of sorting algo

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ import argparse
 # import user defined functions
 from src.renderDicom import plotDicom as plotDicom
 
+def sort_dicom_list(dicom_list):
+    """
+    INPUTS:
+        dicom_list: an unsorted list of dicom objects
+    OUTPUT:
+        sorted list of dicom objects based off of dicom InstanceNumber
+    """
+
+    # test that all elements of the list are dicom objects
+    if not all([True if type(x) == dicom.dataset.FileDataset else False for x in dicom_obj] == False):
+        raise AssertionError("Not all elements are dicom images")
+
+
+
+
+
 # main
 def main():
     # pass command line args
@@ -27,6 +43,7 @@ def main():
 
     # read dicom files
     dicom_obj = [dicom.read_file(x, force=True) for x in dicom_files]
+    import pdb; pdb.set_trace()
     dc = dicom_obj[6]
 
     # render
