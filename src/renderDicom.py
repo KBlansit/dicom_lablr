3cab1b684a988e6551dddd8d00097cf9a94d1881
Kevin Blansit <kevin.blansit@gmail.com>
2017-04-20 00:26:05 -0700

    added visibility to dots

--- src/renderDicom.py
+++ src/renderDicom.py
@@ -25,12 +25,13 @@ class RenderDicomSeries:
         self.ax = ax
         self.dicom_lst = dicom_lst
 
-        # set all circle_data and location_data as None
+        # set all circle_data and slice as None
         self.circle_data = {x: None for x in valid_location_types}
-        self.location_data = {x: None for x in valid_location_types}
+        self.circle_location = {x: None for x in valid_location_types}
+        self.slice_location = {x: None for x in valid_location_types}
 
         # initialize current selections
-        self.curr_user_selection = None
+        self.curr_selection = None
         self.curr_idx = 0
 
         # render to image
@@ -86,6 +87,15 @@ class RenderDicomSeries:
         self.x_max = self.ax.get_xlim()[1]
         self.y_max = self.ax.get_ylim()[0]
 
+        # determine if need to remove or re-draw circles
+        for x in valid_location_types:
+            if self.slice_location[x] == new_idx:
+                self.circle_data[x].set_visible(True)
+            elif self.slice_location[x] == None:
+                pass
+            elif self.circle_data[x] is not None:
+                self.circle_data[x].set_visible(False)
+
         # update view
         self.ax.figure.canvas.draw()
 
@@ -93,15 +103,23 @@ class RenderDicomSeries:
         """
         """
         # return if nothing is selected
-        if self.curr_user_selection is None:
+        if self.curr_selection is None:
             return
 
-        # select the
-        # small circle
-        inner_circ = Circle((event.xdata, event.ydata), 1, edgecolor='red', fill=True)
-        self.ax.add_patch(inner_circ)
+        # test if already populated data to reset
+        if self.circle_data[self.curr_selection] is not None:
+            # remove old circle
+            self.circle_data[self.curr_selection].remove()
 
-        self.curr_collection.add_circle_location(self.curr_selection, inner_circ)
+        # create circle object
+        circ = Circle((event.xdata, event.ydata), 1, edgecolor='red', fill=True)
+        self.circle_data[self.curr_selection] = circ
+        self.circle_data[self.curr_selection].PLOTTED = True
+        self.ax.add_patch(circ)
+
+        # add slice_location and circle location information
+        self.slice_location[self.curr_selection] = self.curr_idx
+        self.circle_location[self.curr_selection] = circ.center
 
         # draw image
         self.ax.figure.canvas.draw()
@@ -110,7 +128,7 @@ class RenderDicomSeries:
         """
         """
         # return if nothing is selected
-        if self.curr_user_selection is None:
+        if self.curr_selection is None:
             return
 
     def _on_key_press(self, event):
@@ -142,15 +160,18 @@ class RenderDicomSeries:
             if hasattr(self, "curr_selection"):
                 if x == self.curr_selection:
                     curr_status[ind] = "X"
-                elif self.location_data[x] is not None:
-                    curr_status[ind] = "slice: " + str(self.location_data[x])
-                usr_msg = "".join([x+" ["+y+"] " for x,y in zip(valid_location_types, curr_status)]) + "\r"
-            else:
-                usr_msg = "".join([str(x) + " [] " for x in valid_location_types]) + "\r"
+                elif self.slice_location[x] is not None:
+                    curr_status[ind] = "slice: " + str(self.slice_location[x])
 
-            # write message
-            sys.stdout.write(usr_msg)
-            sys.stdout.flush()
+        # concatenate message
+        if hasattr(self, "curr_selection"):
+            usr_msg = "".join([x+" ["+y+"] " for x,y in zip(valid_location_types, curr_status)]) + "\r"
+        else:
+            usr_msg = "".join([str(x) + " [] " for x in valid_location_types]) + "\r"
+
+        # write message
+        sys.stdout.write(usr_msg)
+        sys.stdout.flush()
 
     def _add_circle_location(self, location_type):
         """
@@ -159,16 +180,6 @@ class RenderDicomSeries:
         if location_type not in valid_location_types:
             raise AssertionError("Location type not in predefined location types")
 
-        # test if already populated data to reset
-        if self.circle_data[location_type] is not None:
-            # remove old circle
-            self.location_data[location_type].remove()
-
-            # add new circle
-            self.circle_data[location_type] = circle
-        else:
-            # add new circle
-            self.circle_data[location_type] = circle
 
     def _next_image(self):
         if self.curr_idx == len(self.dicom_lst) - 1:
