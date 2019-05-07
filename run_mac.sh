784563b8e6eaf1ce257fbd50f086b9fdf067addc
Kevin Blansit <kevin.blansit@gmail.com>
2019-05-06 19:47:00 -0700

    changed script + some automator stuff

--- run_mac.sh
+++ run_mac.sh
@@ -1,2 +1,3 @@
-cd /Users/kevinblansit/Documents/Programming/ca_lablr
-/usr/local/bin/python3  -W ignore main.py -s settings/ca_settings.yaml -v data/test_output  -p $1
+cd /Users/kevinblansit/Documents/Programming/ca_lablr/ca_lablr
+source ../bin/activate
+python3  -W ignore main.py -s settings/ca_settings.yaml -v data/test_output  -p $1
